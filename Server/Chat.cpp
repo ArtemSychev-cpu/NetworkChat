6917d084decf398fd43ff466a54321fd55c25203
ArtemSychev-cpu <125487729+ArtemSychev-cpu@users.noreply.github.com>
2023-07-28 18:09:41 +0300

    Update Chat.cpp

--- Server/Chat.cpp
+++ Server/Chat.cpp
@@ -1,1 +1,18 @@
+#include "Chat.h"
+#include "Server.h"
 
+void Chat::createChat()
+{
+	Server& server = Server::ser();
+	ChatWork = true;
+}
+
+bool Chat::ChatWork()
+{
+	return ChatWork;
+}
+
+void Chat::closeChat()
+{
+	ChatWork = false;
+	close(Server::ser().getSocket_file_descriptor());
