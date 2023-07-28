87792313008f39ea41440f53395643079e0a4ed0
ArtemSychev-cpu <125487729+ArtemSychev-cpu@users.noreply.github.com>
2023-07-28 18:04:19 +0300

    Update Chat.cpp

--- Client/Chat.cpp
+++ Client/Chat.cpp
@@ -1,1 +1,19 @@
+#include "Chat.h"
+#include "Client.h"
 
+void Chat::createChat()
+{
+	Client& client = Client::cli();
+	ChatWorker = true;
+}
+
+bool Chat::ChatWorker()
+{
+	return ChatWorker;
+}
+
+void Chat::closeChat()
+{
+	ChatWorker = false;
+	close(Client::cli().getSocket_file_descriptor());
+}
